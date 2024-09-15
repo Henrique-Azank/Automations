505a03c248ac9860cd02626900d01ba1d37ad888
Henrique Azank dos Santos <102073359+Henrique-Azank@users.noreply.github.com>
2024-09-15 11:12:06 -0300

    feat: initial scripts upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# Automation Commands
+
+This repository contains a collection of useful automation commands developed for both Bash and PowerShell environments.
+
+## Contents
+
+- [Bash Commands](#bash-commands)
+- [PowerShell Commands](#powershell-commands)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Bash Commands
+
+Here you will find various Bash scripts to automate common tasks.
+
+## PowerShell Commands
+
+This section includes PowerShell scripts designed to streamline your workflow.
+
+## Usage
+
+To use these commands, clone the repository and navigate to the appropriate directory:
+
+```sh
+git clone https://github.com/yourusername/automation-commands.git
+cd automation-commands
+```
+
+### Running Bash Scripts
+
+```sh
+./script-name.sh
+```
+
+### Running PowerShell Scripts
+
+```powershell
+.\script-name.ps1
+```
+
+## Contributing
+
+Contributions are welcome! Please fork the repository and submit a pull request.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ start_ubuntu.ps1
@@ -0,0 +1,67 @@
+
+# Simple Script for starting an Ubuntu container with Docker Desktop using PowerShell
+
+# Used to mount a local directory to the container as a volume when you need to work with files in the container
+# or for when you just want to use bash in your Windows machine
+
+# Try to get the docker desktop service
+$dockerDesktopServiceName = "Docker Desktop"
+$dockerDesktopPath = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+# Check if the service is running
+$retries = 0
+while ($retries -lt 3) {
+    Write-Output "Checking if Docker Desktop is running"
+    try {
+        # If the service is up, the loop is ended
+        Get-Process -Name $dockerDesktopServiceName -ErrorAction Stop | Out-Null
+        Write-Output "Docker Desktop is already running"
+        break
+    }
+    catch {
+        # Trying to activate the service
+        Write-Output "Docker Desktop is not running ... activating it"
+        Start-Process -FilePath $dockerDesktopPath
+        Start-Sleep -Seconds 5
+        Write-Output "Docker Desktop is now running"
+    }
+    $retries++
+}
+
+# Define the local Windows directory and the container directory
+$localDir = "C:/Henrique"
+$containerDir = "/mnt/henrique"
+
+# Define the container name
+$containerName = "my-ubuntu-container"
+$dockerImage = "ubuntu:latest"
+
+# Check if the container already exists
+$containerExists = docker ps -a --filter "name=$containerName" --format "{{.Names}}" | ForEach-Object { $_ -eq $containerName }
+
+# If the container exists, we need to either start it or attach to it
+if ($containerExists) {
+    Write-Output "The container $containerName already exists"
+
+    # Check if the container is running
+    $containerRunning = docker ps --filter "name=$containerName" --format "{{.Names}}" | ForEach-Object { $_ -eq $containerName }
+
+    # If the contyainer is alrerady running, attach to it
+    if ($containerRunning) {
+        # Attach to the running container
+        docker exec -it $containerName bash
+    } else {
+        # Start the stopped container and attach to it
+        docker start $containerName
+        docker exec -it $containerName bash
+    }
+
+}
+# If the container does not exist, we need to create it and attach to it
+else {
+    Write-Output "The container $containerName does not exist"
+    Write-Output "Creating the container $containerName"
+    docker run -it --name $containerName --mount type=bind,source="$localDir",target="$containerDir" $dockerImage bash
+}
+
+
